9c7eeb661f3fef7147f2346c086eb804f755f1e3
BrandonComins <brandcomin@gmail.com>
2020-08-28 22:23:41 -0700

    make an item spawner

---
+++ Scripts/Spawners/SpawnerData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Spawner.asset", menuName = "Spawners/Spawner")]
+public class SpawnerData : ScriptableObject{
+
+    public GameObject itemToSpawn;
+
+    public int minSpawnAmount;
+    public int maxSpawnAmount;
+
+}
